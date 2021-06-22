18b9a0499b836a3db78485d0560370fd76477faf
Warren <moorewarrenthanyours@gmail.com>
2021-06-22 12:58:36 +1000

    Initial Commit

---
+++ LoopsChallengeAverage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopsChallengeAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
